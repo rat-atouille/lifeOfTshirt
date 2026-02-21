f8de7e384d6f3141ba8f6fc086f4df186ec8d3c2
Heena <heenap0113@gmail.com>
2026-02-21 17:49:18 -0500

    Initial Commit

---
+++ Life of a T-Shirt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Life of a T-Shirt
+//
+//  Created by Heena Pong on 2026-02-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Life of a T-Shirt/Life_of_a_T_ShirtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_of_a_T_ShirtApp.swift
+//  Life of a T-Shirt
+//
+//  Created by Heena Pong on 2026-02-21.
+//
+
+import SwiftUI
+
+@main
+struct Life_of_a_T_ShirtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
